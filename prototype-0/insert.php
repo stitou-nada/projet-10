87cc2d57319515c203fe8c427d368b28e914643f
stitou-nada <nadastito1087@gmail.com>
2022-02-14 11:52:45 +0000

    add

--- prototype-0/insert.php
+++ prototype-0/insert.php
@@ -1,59 +1,47 @@
 <?php 
     if ( !empty($_POST)) { 
         // post values
-        $fname  = $_POST['fname'];
-        $lname  = $_POST['lname'];
-        $age    = $_POST['age'];
-        $gender = $_POST['gender'];
-		$person = array($fname, $lname, $age, $gender); 
+        $LastName = $_POST['LastName'];
+        $FirstName    = $_POST['FirstName'];
+        $Age    = $_POST['Age'];
       
-		$file = file_get_contents('people.json');
-		$data = json_decode($file, true);
-		// unset($_POST["add"]);
-		// $data["records"] = array_values($data["records"]);
+		$person = array($FirstName, $LastName , $Age); 
+      
+		$fichier = file_get_contents('person.json');
+		$data = json_decode($fichier);
+	
 		array_push($data, $person);
-		file_put_contents("people.json", json_encode($data));
+		file_put_contents("person.json", json_encode($data));
 		header("Location: index.php");
 
     }
 ?>
 <div>
         <div>
-		<div><h3>Create a User</h3>
+		<div><h3>ajoute</h3>
         <form method="POST" action="">
 			<div>
-				<label for="inputFName">First Name</label>
-				<input type="text" required="required" id="inputFName" name="fname" placeholder="First Name">
-				<span></span>
+				<label >First Name</label>
+				<input type="text"  name="FirstName" >
 			</div>
 			
 			<div>
-				<label for="inputLName">Last Name</label>
-				<input type="text" required="required" id="inputLName" name="lname" placeholder="Last Name">
-        		<span></span>
+				<label >Last Name</label>
+				<input type="text"  name="LastName" >
 			</div>
 			
 			<div>
-				<label for="inputAge">Age</label>
-				<input type="number" required="required" class="form-control" id="inputAge" name="age" placeholder="Age">
-				<span></span>
+				<label >Age</label>
+				<input type="text"  name="Age" >
 			</div>
-				<div class="form-group">
-					<label for="inputGender">Gender</label>
-					<select class="form-control" required="required" id="inputGender" name="gender" >
-						<option>Please Select</option>
-						<option value="Male">Male</option>
-						<option value="Female">Female</option>
-					</select>
-					<span></span>
-        		</div>
-    
+			
+			
 			<div class="form-actions">
-					<button type="submit">Create</button>
+					<button type="submit">ajoute</button>
 					<a href="index.php">Back</a>
 			</div>
 		</form>
         </div></div>        
 </div>
 </body>
-</html> 
+</html>
