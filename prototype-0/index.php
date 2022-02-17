d2cf38eadb6a68689c507585bd376bf98844a4dd
stitou-nada <nadastito1087@gmail.com>
2022-02-17 12:44:24 +0000

    add

--- prototype-0/index.php
+++ prototype-0/index.php
@@ -1,31 +1,43 @@
-
 <?php
-$fichier = file_get_contents('person.json');
-$data = json_decode($fichier);
+
+$ajouterFicher = file_get_contents("personnes.json");
+$data = json_decode($ajouterFicher);
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-		<a href="insert.php"><i></i> Insert Data</a>
-			<table>
-				<tr>
-					<th>Prenom</th>
-					<th>nom</th>
-					<th>Age</th>
-				</tr>		
-				<?php foreach ($data as $person)
-				      
-				
-				?>
-				<tr>
-					
-					<td><?php echo $person[0]; ?></td>
-					<td><?php echo $person[1]; ?></td>
-					<td><?php echo $person[2]; ?></td>
-					
-					
-				</tr>
-				
-			</table>
-	
+    <div>
+        <a href="insert.php">ajoute</a>    
+    </div>
+    <table>
+    <tr>
+                <th>Prenom</th>
+                <th>Nom</th>
+                <th>Age</th>
+            </tr>
+ <?php
+  foreach($data as $value)
 
+ ?>
+ <tr>
+    <td><?php echo $value[1] ?></td>
+    <td><?php echo $value[2]?></td>
+    <td><?php echo $value[3]?></td>
+ <td>
+
+ </td>
+ </tr>
+
+
+ </table>
 </body>
 </html>
